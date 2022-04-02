3e1d81e2fc81d4fdf2ef2675c5c786d481204480
chenshengmai <chenbaize999@gmail.com>
2022-04-01 23:52:56 -0400

    simply construct model.py

--- code/model.py
+++ code/model.py
@@ -1,12 +1,14 @@
 import math
 import torch
 import numpy
+from torch import nn
 from torch.autograd.grad_mode import F
 
 
-class LanguageModel():
-    def __init__(self, dim_model, dim_ff, vocab, num_layers, num_head, dropout):
-        self.embd = Embeddings(dim_model, vocab)
+class LanguageModel(nn.Module):
+    def __init__(self, dim_model, dim_ff, vocab_size, num_layers, num_head, dropout):
+        super(LanguageModel, self).__init__()
+        self.embd = Embeddings(dim_model, vocab_size)
         self.pos_encoding = PositionalEncoding(dim_model, dropout)
         self.decoder = Decoder(dim_model, num_layers, num_head, dim_ff, dropout)
         self.output_embd = OutputEmbeddings(dim_model, vocab)
@@ -19,11 +21,38 @@ class LanguageModel():
 
         return x
 
-class Embeddings():
-    def __init__(self):
+class Embeddings(nn.Module):
+    def __init__(self, dim_model, vocab_size):
+        super(Embeddings, self).__init__()
+        self.embd_layer = nn.Embedding(vocab_size, dim_model)
+        self.dim_model = dim_model
 
-class
+    def forward(self, x):
+        return self.embd_layer(x) * math.sqrt(self.dim_model)
 
+class PositionalEncoding():
+    def __init__(self, dim_model, dropout):
+        super(PositionalEncoding, self).__init__()
+        self.dim_model = dim_model
+        self.dropout = nn.Dropout(dropout)
+        self.pe = torch.zeros(5000, dim_model)
+        for pos in range(5000):
+            for i in range(dim_model):
+                if i % 2 == 0:
+                    self.pe[pos, i] = math.sin(pos / math.pow(10000, (2.0*i)/dim_model))
+                else:
+                    self.pe[pos, i] = math.cos(pos / math.pow(10000, (2.0*i)/dim_model))
+
+    def forward(self, x): #0 batch_size, 1 seq_len, 2, dim_model, pe, 0 5000, 1, dim_model
+        x = x + self.pe[:x.size(1), :].unsqueeze(0)
+        return x
+
+def main():
+    position = PositionalEncoding(1024, 0.1)
+    print(position.pe[:10, :4])
+
+if __name__ == "__main__":
+    main()
 
 
 
